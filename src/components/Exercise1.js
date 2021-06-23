084ae0c49d5bdd049cc8a131fef7de58759378a1
khaleel esa <khaleel_esa@hotmail.com>
2021-06-23 15:57:47 +0300

    finished ex1,ex2

--- src/components/Exercise1.js
+++ src/components/Exercise1.js
@@ -1,24 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Exercise1 extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "",
+      age: "",
+    };
+  }
 
-    constructor() {
-        super()
-        this.state = {
-            name: "",
-            age: ""
-        }
-    }
+  updateName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
 
-    render() {
-        return (
-            <div>
-                <input id="name-input" />
-                <input id="age-input" />
-                <button>Go to Bar</button>
-            </div>
-        );
-    }
+  updateAge = (event) => {
+    this.setState({
+      age: event.target.value,
+    });
+  };
+
+  alertText = () => {
+    alert(
+      "wolcome " +
+        this.state.name +
+        " you are: " +
+        this.state.age +
+        " years old"
+    );
+  };
+  render() {
+    return (
+      <div>
+        <input
+          id="name-input"
+          value={this.state.name}
+          onChange={this.updateName}
+        />
+        <input
+          id="age-input"
+          value={this.state.age}
+          onChange={this.updateAge}
+        />
+        <button onClick={this.alertText}>Go to Bar</button>
+      </div>
+    );
+  }
 }
 
 export default Exercise1;
--- src/components/Exercise2.js
+++ src/components/Exercise2.js
@@ -1,23 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Exercise2 extends Component {
-
-constructor() {
-    super()
+  constructor() {
+    super();
     this.state = {
-        name: "",
-        fruit: ""
-    }
-}
+      name: "",
+      fruit: "",
+    };
+  }
 
-render() {
+  updateName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
+
+  handleChange = (event) => {
+    this.setState({ fruit: event.target.value }, function () {
+      console.log(
+        "name: " + this.state.name + " , Fruite: " + this.state.fruit
+      );
+    });
+  };
+  render() {
     return (
-        <div>
-            <input id="name-input" />
-            <select id="select-input"></select>
-        </div>
+      <div>
+        <input
+          id="name-input"
+          value={this.state.name}
+          onChange={this.updateName}
+        />
+        <select
+          id="select-input"
+          value={this.state.fruit}
+          onChange={this.handleChange}
+        >
+          <option value="banana">banana</option>
+          <option value="apple">apple</option>
+          <option value="grap">grap</option>
+          <option value="watermelon">watermelon</option>
+        </select>
+      </div>
     );
-}
+  }
 }
 
 export default Exercise2;
